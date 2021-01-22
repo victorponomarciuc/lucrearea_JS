20a59e8faa9fbdc444aae01655cdc8467e7f5419
victorponomarciuc <71686962+victorponomarciuc@users.noreply.github.com>
2021-01-22 16:10:10 +0200

    adaugarea contorului + si -

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
-var i = 1;                
-function myLoop() {         
-  setTimeout(function() { 
-    el = document.createElement('li');  
-    el.innerHTML = i;  
-    document.getElementById('ul__list').appendChild(el); 
-    i++;                    
-    if (i < 21) {          
-      myLoop();              
-    }                       
-  }, 2000)
+var i = 1;
+function myLoop() {
+    setTimeout(function () {
+        el = document.createElement('li');
+        el.innerHTML = i;
+        document.getElementById('ul__list').appendChild(el);
+        i++;
+        if (i < 21) {
+            myLoop();
+        }
+    }, 2000)
 }
-myLoop(); 
+myLoop();
+
+
+var count = 0;
+var countEl = document.getElementById("count");
+function plus() {
+    count++;
+    countEl.value = count;
+    if (count > 9){
+        alert('interval interzis')
+    }
+    if (count < 10) {
+         countEl.value = count;
+    }
+}
+function minus() {
+    if (count > 0) {
+        count--;
+        countEl.value = count;
+    }
+    if (count <0){
+        alert('interval interzis')
+    }
+}
